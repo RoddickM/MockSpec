41183df816d9f5feedfc0aa9071d7624fd494771
RoddickM <98747226+RoddickM@users.noreply.github.com>
2023-01-13 15:25:18 +0000

    Add files via upload

---
+++ toka_fit/main_website/personal_trainer.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="styles/home_styles.css" />
+        <title>Home</title>
+    </head>
+    <body>
+        <?php include('includes/navbar.html') ?>
+        <section class="hero-image">
+            <div class="hero-text">
+                <p class="hero-title">TOKA FITNESS</p>
+                <button class="join-now">JOIN NOW</button>
+            </div>
+        </section>
+
+        <section class="card-container">
+            <div class="card">
+                <div class="card-content">
+                    <h4><b>LEARN SO MUCH</b></h4>
+                    <p>Explore our selection of high quality 
+                        videos teaching you fitness and healthy living tips.</p>
+                    <button class="card-button">WATCH HERE</button>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-content">
+                    <h4><b>TRAIN</b></h4>
+                    <p>Try our award winning personal trainers 
+                        and start changing your life today.</p>
+                    <button class="card-button">PERSONAL TRAINER</button>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-content">
+                    <h4><b>KNOW MORE</b></h4>
+                    <p>Have a look at our in depth articles 
+                        discussing healthy living and fitness.</p>
+                    <button class="card-button">READ HERE</button>
+                </div>
+            </div>
+        </section>
+        <?php include('includes/footer.html') ?>
+    </body>
+</html>
